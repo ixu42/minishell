addd5f96b421241ee75f74399adac9d529ea50ed
Ivy Xu <ivy21418@gmail.com>
2024-02-15 10:06:42 +0200

    structs updated, etc

--- minishell.h
+++ minishell.h
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ixu <ixu@student.hive.fi>                  +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/09 10:37:05 by ixu               #+#    #+#             */
-/*   Updated: 2024/02/09 13:06:47 by ixu              ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
@@ -20,15 +8,75 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 
-// STDIN_FILENO, STDOUT_FILENO, STDERR_FILENO
+/* write, close, dup2, fork, pipe, access, execve, unlink
+STDIN_FILENO, STDOUT_FILENO, STDERR_FILENO */
 # include <unistd.h>
 
 // EXIT_FAILURE, EXIT_SUCCESS, NULL
 # include <stdlib.h>
 
+// wait
+# include <sys/wait.h>
+
 // macros for (error) messages
 # define ERR_WRITE "write error"
 # define ERR_ARGS "invalid number of arguments"
 # define USAGE "ex. usage: ./minishell"
+
+// Parsed command representation
+#define EXEC  1
+#define REDIR 2
+#define PIPE  3
+#define LIST  4
+
+#define MAXARGS 10
+
+# define ERR_PIPE "pipe error"
+# define ERR_FORK "fork error"
+# define ERR_WAITPID "waitpid error"
+// # define EXIT_CMD_PERM_ERR 126
+# define EXIT_CMD_NOT_FOUND 127
+
+typedef struct s_data
+{
+	char	*buf;
+	char	**envs;
+}	t_data;
+
+typedef struct s_cmd
+{
+	int	type;
+}	t_cmd;
+
+typedef struct s_execcmd
+{
+	int		type;
+	char	*argv[MAXARGS];
+	char	*eargv[MAXARGS];
+}	t_execcmd;
+
+typedef struct s_redircmd
+{
+	int		type;
+	t_cmd	*cmd;
+	char	*file;
+	char	*efile;
+	int		mode;
+	int		fd;
+}	t_redircmd;
+
+typedef struct s_pipecmd
+{
+	int		type;
+	t_cmd	*left;
+	t_cmd	*right;
+}	t_pipecmd;
+
+typedef struct s_listcmd
+{
+	int		type;
+	t_cmd	*left;
+	t_cmd	*right;
+}	t_listcmd;
 
 #endif
