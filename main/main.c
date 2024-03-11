3db103ba04c690e5e0adfe4fb86f90aa3c394c40
Ivy Xu <xyan21418@gmail.com>
2024-03-11 23:29:37 +0200

    fixed text wrapping; signals work in progress

--- main/main.c
+++ main/main.c
@@ -30,16 +30,17 @@ int	is_valid_buf(char *buf)
 	return (0);	
 }
 
-// int	signum;
-
-// void	signal_handler(int signum)
-// {
-// 	printf("Received signal %d\n", signum);
-// 	if (signum == SIGINT)
-// 	{
-
-// 	}
-// }
+void	signal_handler(int signum)
+{
+	// printf("Received signal %d\n", signum);
+	if (signum == SIGINT)
+	{
+		printf("\n");
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
+	}
+}
 
 // sig_t	signal(int sig, sig_t func)
 
@@ -47,7 +48,7 @@ int	main(int argc, char **argv, char **envp)
 {
 	t_data	data;
 	t_cmd	*cmd;
-	int 	status;
+	int		status;
 
 	// ------ print out envp ------
 	// for (int k = 0; envp[k] != NULL; k++)
@@ -56,14 +57,18 @@ int	main(int argc, char **argv, char **envp)
 	(void)argv;
 	validate_args(argc);
 	data_init(&data, envp);
-	while (data.status_code >= 0) 
+	while (data.status_code >= 0)
 	{
-		// signal(signum, signal_handler);
-		data.buf = readline("\033[0;32mLiteShell$ \033[0m"); // free
+		struct sigaction	sa;
+		sa.sa_handler = &signal_handler;
+		// sa.sa_flags = SA_RESTART;
+		sigaction(SIGINT, &sa, NULL);
+		// signal(SIGINT, signal_handler);
+		// data.buf = readline("\033[0;32mLiteShell$ \033[0m"); // free
+		data.buf = readline("LiteShell$ "); // free. discuss if all color formatting should be removed
 		if (data.buf == NULL) // Check for EOF (Ctrl+D)
 		{
-			// check out readline funcs
-			printf("exit"); // bash on MacOS prints prompt + exit
+			printf("\033[A\033[11Cexit\n");
 			break ;
 		}
 		if (is_valid_buf(data.buf))
